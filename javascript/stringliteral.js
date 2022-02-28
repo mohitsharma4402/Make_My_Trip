65a25dbfffb234d9fd88f01431179176c2b4b26c
MOHIT SHARMA <you@example.com>
2022-02-28 21:14:43 +0530

    photos adding

--- javascript/stringliteral.js
+++
@@ -1,8 +0,0 @@
-let name = "ABCD";
-let ind = "Bollywood";
-let type = "Dance";
-let rating = 4.0;
-
-//es5
-let a = `An $(name) is a $(type) movie with rating of $(rating) and from ind $(ind) `
-console.log(>>>a,a)
